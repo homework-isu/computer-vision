691ffdaac66b4b9b9c0675993c0d007d02356a9d
Danila-Ivashchenko <105924943+Danila-Ivashchenko@users.noreply.github.com>
2023-11-10 11:35:12 +0800

    Цепочечный алгоритм

--- chain/main.py
+++ chain/main.py
@@ -27,7 +27,10 @@ def is_perimeter_point(y, x, img):
 
     for dy, dx in dirs:
         neighbor_y, neighbor_x = y + dy, x + dx
-        if 0 <= neighbor_x < img.shape[1] and 0 <= neighbor_y < img.shape[0] and img[neighbor_y, neighbor_x] == 0:
+        if (0 <= neighbor_x < img.shape[1] and
+            0 <= neighbor_y < img.shape[0] and
+            img[neighbor_y, neighbor_x] == 0) or \
+           (neighbor_x == img.shape[1] or neighbor_y == img.shape[0]):
             return True
 
     return False
@@ -76,7 +79,31 @@ def chain(img, label=1):
     return dirs, perimeter
 
 
-data = np.array(
+def are_equal_chains(ch1, ch2):
+    ch1_copy = ch1.copy()
+    n = len(ch1_copy)
+
+    while ch1_copy != ch2 and n > 0:
+        ch1_copy = [ch1_copy[i - 1] for i in range(len(ch1_copy))]
+        n -= 1
+    return ch1_copy == ch2
+
+
+data1 = np.array(
+    [
+        [0, 0, 1, 1, 0, 0, 0],
+        [0, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 0],
+    ]
+)
+
+data2 = np.array(
     [
         [0, 0, 1, 1, 0, 0, 0],
         [0, 1, 1, 1, 1, 0, 0],
@@ -90,10 +117,23 @@ data = np.array(
     ]
 )
 
-dirs, perimeter = chain(data, 1)
-print("perimeter points:", perimeter)
-print("nums of dirs:", dirs)
+print(data1.shape)
+dirs1, perimeter1 = chain(data1, 1)
+dirs2, perimeter2 = chain(data2, 1)
+print("perimeter points of data 1:", perimeter1)
+print("nums of dirs of data 1:", dirs1)
 
-plt.imshow(data)
-plt.scatter([point[1] for point in perimeter], [point[0] for point in perimeter], c='r')
+print("perimeter points of data 2:", perimeter2)
+print("nums of dirs of data 2:", dirs2)
+
+print(are_equal_chains(dirs1, dirs2))
+
+plt.subplots(nrows=1, ncols=2)
+plt.subplot(1, 2, 1)
+plt.imshow(data1)
+plt.scatter([point[1] for point in perimeter1], [point[0] for point in perimeter1], c='r')
+
+plt.subplot(1, 2, 2)
+plt.imshow(data2)
+plt.scatter([point[1] for point in perimeter2], [point[0] for point in perimeter2], c='r')
 plt.show()
